0f9daed5c8576bb5fc6d1f2a93d76b67e6befdeb
zahrabenmohamed <benmedzahra20@gmail.com>
2023-06-02 17:50:53 +0100

    update settings

--- pages/pages/setting/index.js
+++ pages/pages/setting/index.js
@@ -1,5 +1,4 @@
 import React , {useState} from 'react';
-import Link from 'next/link';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 
@@ -10,48 +9,61 @@ const EmptyPage = () => {
 
     return (
        <>
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <div className='card-header'>
-                        <h5>User</h5>
-                    </div>
-                    <hr className='card-separator' />
-                    <div className='card-body'>
-                        <span>First Name </span>
-                    <InputText  />
+      <div className="grid">
+    <div className="col-12">
+        <div className="card">
+            <div className='card-header'>
+                <h5>User</h5>
+            </div>
+            <hr className='card-separator' />
+            <div className='card-body'>
+                <div className="input-group">
+                    <span style={{ marginRight: '10px' }}>First Name</span>
+                    <InputText style={{ marginRight: '10px' }}/>
                     <Button label="Submit" />
-                    <span>Last Name</span>
-                    <InputText  />
+                </div>
+                <div className="input-group" style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+                    <span style={{ marginRight: '10px' }}>Last Name</span>
+                    <InputText style={{ marginRight: '10px' }} />
                     <Button label="Change Name" />
-                    <span>Email</span>
-                    <InputText  />
-                    <Button label="Change Email" />                    
-                    </div>
+                </div>
+                <div className="input-group" style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+                    <span style={{ marginRight: '10px' }}>Email</span>
+                    <InputText style={{ marginRight: '10px' }} />
+                    <Button label="Change Email" />
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <div className='card-header'>
-                        <h5>Password</h5>
-                    </div>
-                    <hr className='card-separator' />
-                    <div className='card-body'>
-                        <span>Existing Password</span>
-                    <InputText  />
-                    <span>New Password</span>
-                    <InputText  />
-                    <p>Password must be at least 6 characters and contain a mixture of letters, numbers and punctuation.</p>
-                    <span>Confirm New Password</span>
-                    <InputText  />
-                    <Button label="Change Password" />                      
-                    </div>
+<div className="grid">
+    <div className="col-12">
+        <div className="card">
+            <div className='card-header'>
+                <h5>Password</h5>
+            </div>
+            <div className='card-body'>
+                <div className="input-group">
+                    <span>Existing Password</span>
+                    <InputText />
                 </div>
+                <h2></h2>
+                <div className="input-group">
+                    <span>New Password</span>
+                    <InputText />
+                </div>
+                <p>Password must be at least 6 characters and contain a mixture of letters, numbers, and punctuation.</p>
+                <div className="input-group">
+                    <span>Confirm New Password</span>
+                    <InputText />
+                </div>
+                <Button label="Change Password" />
             </div>
         </div>
+    </div>
+</div>
+
        </>
     );
 };
